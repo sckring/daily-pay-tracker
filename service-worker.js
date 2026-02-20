316b8d561195bb11960bb6f5a4b1ef902a848f87
sckring <44453476+sckring@users.noreply.github.com>
2026-02-20 09:13:59 -0500

    added server for push notifications

--- service-worker.js
+++ service-worker.js
@@ -27,15 +27,11 @@ self.addEventListener("notificationclick", event => {
 });
 
 self.addEventListener("push", event => {
-  if (!event.data) return;
-
   const data = event.data.json();
 
-  event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "icon-192.png",
-      badge: "icon-192.png"
-    })
-  );
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "/icon-192.png",
+    badge: "/icon-192.png"
+  });
 });
