39ed79174e671d87e77bd1d69be20906a0ac43b9
sckring <44453476+sckring@users.noreply.github.com>
2026-02-20 14:24:04 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("pay-tracker-v7").then(cache =>
+    caches.open("pay-tracker-v8").then(cache =>
       cache.addAll([
         "./",
         "./index.html",
